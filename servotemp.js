24996dd3de58ee9f278baa7190f38b8352a9aa8d
Philippe Breuils <philippe.breuils@gmail.com>
2012-11-09 18:05:26 +0100

    added express skeleton

--- servotemp.js
+++ servotemp.js
@@ -1,16 +1,36 @@
 var sio = require('socket.io')
   , serial = require('serialport')
-  , express = require('express')
   , child = require('child_process')
   , moment = require('moment');
 
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path');
+
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+app.configure(function(){
+  app.set('port', process.env.PORT || 8088);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.listen(8088);
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
 
 function connectToArduino(callback) {
   // Seems to work on a mac and on a Raspberry Pi
